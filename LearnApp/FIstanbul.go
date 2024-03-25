7dd4c89a97ca316d7522388ba4aa4b2711ff63ce
Derick <charleszhn@hotmail.com>
2024-03-26 00:48:50 +0800

    自定义异常

--- LearnApp/FIstanbul.go
+++ LearnApp/FIstanbul.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func test(x [2]int) {
 	fmt.Printf("x:%p\n", &x)
@@ -55,16 +58,17 @@ func printArr(arr *[5]int) {
 		fmt.Println(i, v)
 	}
 }
+
 //找出数组中和为给定值的两个元素的下标，例如数组[1,3,5,8,7]，
 //找出两个元素之和等于8的下标分别是（0，4）和（1，2）
 
 // 求元素和，是给定的值
-func myTest(a [5]int,target int){
-	for i:=0;i<len(a);i++{
-		other:=target-a[i]
-		for j:=i+1;j<len(a):j++{
-			if a[j]==other {
-				fmt.Printf("(%d,%d)\n",i,j)
+func myTest(a [5]int, target int) {
+	for i := 0; i < len(a); i++ {
+		other := target - a[i]
+		for j := i + 1; j < len(a); j++ {
+			if a[j] == other {
+				fmt.Printf("(%d,%d)\n", i, j)
 			}
 		}
 	}
