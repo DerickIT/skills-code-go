090fc3a3b30d28ecbf1276aadd58e24ed1b9a5b0
Derick <charleszhn@hotmail.com>
2024-03-26 23:12:31 +0800

    定义异常

--- LearnApp/Error/BizError.go
+++ LearnApp/Error/BizError.go
@@ -6,6 +6,41 @@ import (
 	"time"
 )
 
+func test01() {
+	a := [5]int{1, 2, 3, 4, 5}
+	a[1] = 10
+	fmt.Println(a)
+	index := 11
+	a[index] = 11
+	fmt.Println(a)
+}
+
+func getCircleArea(radius float32) (area float32) {
+	if radius < 0 {
+		panic("半径不能为负数")
+	}
+	return 3.14 * radius * radius
+}
+
+func test02() {
+	getCircleArea(-5)
+}
+
+func test03() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("发生错误", err)
+		}
+	}()
+	getCircleArea(-5)
+	fmt.Println("没有执行")
+}
+
+func test04() {
+	test03()
+	fmt.Println("test04执行完毕")
+}
+
 type PathError struct {
 	path       string
 	op         string
@@ -32,6 +67,9 @@ func Open(filename string) error {
 }
 
 func main() {
+
+	test04()
+
 	err := Open("test.txt")
 	switch v := err.(type) {
 	case *PathError:
