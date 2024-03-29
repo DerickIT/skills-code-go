0cd58cf9312f641dfb86b32653d31e9de23355db
Derick <charleszhn@hotmail.com>
2024-03-29 22:46:04 +0800

    匿名函数

--- LearnApp/Func/func1.go
+++ LearnApp/Func/func1.go
@@ -26,7 +26,21 @@ func (m *Manageer) ToString() string {
 }
 
 func main() {
+
+	pos, neg := addr(), addr()
+	for i := 0; i < 10; i++ {
+		fmt.Println(pos(i), neg(-2*i))
+	}
+
 	m := Manageer{User{1, "Tom"}, "Administrator"}
 	fmt.Println(m.ToString())
 	fmt.Println(m.User.ToString())
+}
+
+func addr() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
